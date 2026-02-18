c252495dd62aee434f04a03c5ee855183e1a5930
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2026-02-18 18:19:10 +0800

    Home: Section 3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/CommonStyles.css';
 import '../styles/HomeStyles.css';
 import homesection2 from '../imgs/home-section2.png';
+import homesection3 from '../imgs/home-section3.png';
 
 const Home = () => {
   return (
@@ -16,7 +17,16 @@ const Home = () => {
 
       <section className='home-section2'>
         <h1 className='semibold' style={{ textAlign: 'center' }}>see what a girl can do with her 4-year degree</h1>
-        <img className='homesection2' src={homesection2} style={{ marginTop: 50 }} alt="Logo" />
+        <img className='homesection2' src={homesection2} style={{ marginTop: 50 }} />
+      </section>
+
+      <section className='home-section3'>
+        <div className='home-section3-left'>
+          <p className='semibold-sectiontitle'>first big girl job experience</p>
+          <h1 className='semibold-title'>wanna know more about my first internship experience at KMC?</h1>
+          <p className='regular-subtitle'>To my mentors and co-interns: Thank you for creating a space where I felt comfortable asking questions and for pushing me to grow beyond my technical bubble.</p>
+        </div>
+        <img className='homesection3' src={homesection3} style={{ marginTop: 50 }} />
       </section>
     </div>
   )

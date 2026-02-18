4816b426b58a98f3873ecef5bb75dec940ce55d5
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2026-02-18 19:18:43 +0800

    Work: Section 1 Responsiveness

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,7 +8,7 @@ const Works = () => {
     <div>
       <section className='work-section1'>
         <h1 className='semibold'>It’s more than just making things look good. It’s about crafting intuitive journeys and meaningful experiences that feel seamless. I love the blend of creativity and logic in UI/UX, and how it empowers me to make a real impact on how people interact with technology.</h1>
-        <p className='regular'>++ seeing users engage with something I designed is incredibly rewarding.</p>
+        <p className='regular-subtitle'>++ seeing users engage with something I designed is incredibly rewarding.</p>
       </section>
 
       <section className='footer'>

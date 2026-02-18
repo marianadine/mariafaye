fe639ae3e02ee9b3149617e6d4b5fd0b069780ae
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2026-02-18 19:08:07 +0800

    Work: Footer

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,6 +10,11 @@ const Works = () => {
         <h1 className='semibold'>It’s more than just making things look good. It’s about crafting intuitive journeys and meaningful experiences that feel seamless. I love the blend of creativity and logic in UI/UX, and how it empowers me to make a real impact on how people interact with technology.</h1>
         <p className='regular'>++ seeing users engage with something I designed is incredibly rewarding.</p>
       </section>
+
+      <section className='footer'>
+        <p className='cursive'>maria nadine faye rufo</p>
+        <p className='semibold-footer'>thank u for scrolling bestie!</p>
+      </section>
     </div>
   )
 }

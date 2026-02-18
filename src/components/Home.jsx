cfe0d20fc713bb45b7264c21bb9d739799dd6667
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2026-02-18 17:53:51 +0800

    Home: Section 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import '../styles/CommonStyles.css';
 import '../styles/HomeStyles.css';
+import homesection2 from '../imgs/home-section2.png';
 
 const Home = () => {
   return (
-    <>
+    <div className='background'>
       <section className='home-section1'>
-        <p className='regular'>Hi, I’m Nadine</p>
+        <p className='regular-black'>Hi, I’m Nadine</p>
         <div className='home-section1-right'>
-          <h1 className='semibold'>— an aspiring UI/UX Designer with a passion for clean design and smooth user experiences.</h1>
-          <p className='regular'>*and lowkey willing to learn backend too</p>
+          <h1 className='semibold-black'>— an aspiring UI/UX Designer with a passion for clean design and smooth user experiences.</h1>
+          <p className='regular-black'>*and lowkey willing to learn backend too</p>
         </div>
       </section>
-    </>
+
+      <section className='home-section2'>
+        <h1 className='semibold' style={{ textAlign: 'center' }}>see what a girl can do with her 4-year degree</h1>
+        <img className='homesection2' src={homesection2} style={{ marginTop: 50 }} alt="Logo" />
+      </section>
+    </div>
   )
 }
 
